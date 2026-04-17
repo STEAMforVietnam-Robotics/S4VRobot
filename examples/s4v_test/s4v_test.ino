482f3f3257e4b37e650f6e8d176a91f24b41135c
manhtienvu-vn <manhtienvu.vn@gmail.com>
2026-04-17 22:56:38 +0700

    S4VRobot.cpp: map() giá trị của tham số speed từ 0-100 về range 0-255; thêm function description đầy đủ.

--- examples/s4v_test/s4v_test.ino
+++ examples/s4v_test/s4v_test.ino
@@ -1,46 +1,76 @@
 #include <S4VRobot.h>
 
+// Tạo 1 robot mới để điều khiển
 S4VRobot Robot;
 
 void setup() {
-  Serial.begin(115200);
-  Robot.begin("S4VRobot");
+    Serial.begin(115200);
+    // Thiết lập thông số cho robot vừa tạo
+    Robot.begin("S4VRobot");
 }
 
 void loop()
 {
+    // Đọc tín hiệu điều khiển từ App trên điện thoại
     Robot.processInput();
+
+    // Nếu nút hình dấu 'X' được nhấn
     if (GamePad.isCrossPressed())
     {
+        // Hạ tay đòn của robot xuống phía dưới
         Robot.arm_downward();
     }
+
+    // Nếu nút hình 'TAM GIÁC' được nhấn
     if (GamePad.isTrianglePressed())
     {
+        // Nâng tay đòn của robot lên phía trên
         Robot.arm_upward();
     }
+
+    // Nếu nút hình 'VUÔNG' được nhấn
     if(GamePad.isSquarePressed())
     {
+        // Thu càng gắp của robot lại
         Robot.grasp();
     }
+
+    // Nếu nút hình 'VÒNG TRÒN' được nhấn
     if(GamePad.isCirclePressed())
     {
+        // Mở càng gắp của robot ra
         Robot.release();
     }
+
+    // Nếu nút 'TRÊN' được nhấn
     if(GamePad.isUpPressed())
     {
-        Robot.go_forward();
-    } else if(GamePad.isDownPressed())
+        // Robot đi thẳng
+        Robot.go_forward(100);
+    } 
+    // Nếu nút 'DƯỚI' được nhấn
+    else if(GamePad.isDownPressed())
     {
-        Robot.go_backward();
-    }else if(GamePad.isLeftPressed())
+        // Robot đi thẳng
+        Robot.go_backward(100);
+    }
+    // Nếu nút 'TRÁI' được nhấn
+    else if(GamePad.isLeftPressed())
     {
-        Robot.turn_left();
-    }else if(GamePad.isRightPressed())
+        // Robot xoay trái
+        Robot.turn_left(80);
+    }
+    // Nếu nút 'PHẢI' được nhấn
+    else if(GamePad.isRightPressed())
     {
-        Robot.turn_right();
-    } else 
-    {
+        // Robot xoay phải
+        Robot.turn_right(80);
+    } 
+    // Nếu tất cả điều kiện nút nhấn bên trên đều không thỏa mãn
+    else {
+        // Robot dừng lại
         Robot.stop();
     }
+    // Cứ mỗi 5 mili giây sẽ kiểm tra tín hiệu điều khiển từ App trên điện thoại một lần
     delay(5);
 }
--- src/S4VRobot.cpp
+++ src/S4VRobot.cpp
@@ -1,10 +1,25 @@
+/**
+ * @file S4VRobot.cpp
+ * @brief Định nghĩa các hàm điều khiển cho Robot S4V và link tới thư viện Dabble App
+ * @author Manh Vu
+ * * @note 
+ * LAST UPDATE: 2024-05-20 10:40PM
+ * - Thay đổi range parameter 'speed' cho các hàm điều khiển robot về dạng 0 - 100%
+ * - Thêm hàm map() parameter 'speed' về range 0 - 255 bên trong hàm (local scope) để thực hiện băm PWM
+ * - Thêm Function Description cho tất cả các hàm
+ * * @version 1.2.0
+ */
+
 #define FROM_DABBLE_LIBRARY
 #include "S4VRobot.h"
 
-
-
 S4VRobot::S4VRobot() : armAngle(ARM_SERVO_MIN_ANGLE), gripperAngle(GRIPPER_SERVO_MIN_ANGLE) {};
 
+/**
+ * @brief Mô tả: Hàm khởi tạo các thông số cho robot (ví dụ: trạng thái các chân Digital và Analog).
+ * Hàm này bắt buộc phải được chạy ở hàm setup() trước vòng lặp loop() để có thể điều khiển robot.
+ * @param name : Tên robot của bạn.
+ */
 void S4VRobot::begin(std::string name)
 {
     Dabble.begin(name);
@@ -19,56 +34,73 @@ void S4VRobot::begin(std::string name)
     pinMode(L298_IN4, OUTPUT);
     pinMode(L298_ENB, OUTPUT);
 
-    // Set initial state of the motors
+    // Thiết lập trạng thái ban đầu của các chân điều khiển động cơ ở mức LOW -> Động cơ không xoay.
     digitalWrite(L298_IN1, LOW);
     digitalWrite(L298_IN2, LOW);
     digitalWrite(L298_IN3, LOW);
     digitalWrite(L298_IN4, LOW);
 
-    // Set initial speed of the motors
-    analogWrite(L298_ENA, MOTOR_SPEED); // Full speed for motor A
-    analogWrite(L298_ENB, MOTOR_SPEED); // Full speed for motor B
+    analogWrite(L298_ENA, MOTOR_SPEED); 
+    analogWrite(L298_ENB, MOTOR_SPEED); 
 }
 
 void S4VRobot::wait()
 {
-    // Wait for the app to connect
+    //Chờ Dabble app thiết lập xong
     delay(1000);
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển robot dừng lại.
+ */
 void S4VRobot::stop()
 {
-    // Stop the motors
     control_motor(LEFT_MOTOR, CLOCKWISE, 0);
     control_motor(RIGHT_MOTOR, CLOCKWISE, 0);
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển robot chạy thẳng.
+ * @param speed: Tốc độ di chuyển của robot (0 - 100%).
+ */
 void S4VRobot::go_forward(uint8_t speed)
 {
-    // Move forward
-    control_motor(LEFT_MOTOR, CLOCKWISE, speed);
-    control_motor(RIGHT_MOTOR, CLOCKWISE, speed);
+    uint8_t mappedSpeed = map(speed, 0, 100, 0, 255);
+    control_motor(LEFT_MOTOR, CLOCKWISE, mappedSpeed);
+    control_motor(RIGHT_MOTOR, CLOCKWISE, mappedSpeed);
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển robot chạy lùi.
+ * @param speed: Tốc độ di chuyển của robot (0 - 100%).
+ */
 void S4VRobot::go_backward(uint8_t speed)
 {
-    // Move backward
-    control_motor(LEFT_MOTOR, COUNTER_CLOCKWISE, speed);
-    control_motor(RIGHT_MOTOR, COUNTER_CLOCKWISE, speed);
+    uint8_t mappedSpeed = map(speed, 0, 100, 0, 255);
+    control_motor(LEFT_MOTOR, COUNTER_CLOCKWISE, mappedSpeed);
+    control_motor(RIGHT_MOTOR, COUNTER_CLOCKWISE, mappedSpeed);
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển robot xoay trái.
+ * @param speed: Tốc độ xoay trái của robot (0 - 100%).
+ */
 void S4VRobot::turn_left(uint8_t speed)
 {
-    // Turn left
-    control_motor(LEFT_MOTOR, COUNTER_CLOCKWISE, speed);
-    control_motor(RIGHT_MOTOR, CLOCKWISE, speed);
+    uint8_t mappedSpeed = map(speed, 0, 100, 0, 255);
+    control_motor(LEFT_MOTOR, COUNTER_CLOCKWISE, mappedSpeed);
+    control_motor(RIGHT_MOTOR, CLOCKWISE, mappedSpeed);
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển robot xoay phải.
+ * @param speed: Tốc độ xoay phải của robot (0 - 100%).
+ */
 void S4VRobot::turn_right(uint8_t speed)
 {
-    // Turn right
-    control_motor(LEFT_MOTOR, CLOCKWISE, speed);
-    control_motor(RIGHT_MOTOR, COUNTER_CLOCKWISE, speed);
+    uint8_t mappedSpeed = map(speed, 0, 100, 0, 255);
+    control_motor(LEFT_MOTOR, CLOCKWISE, mappedSpeed);
+    control_motor(RIGHT_MOTOR, COUNTER_CLOCKWISE, mappedSpeed);
 }
 
 void S4VRobot::wait_app_connection()
@@ -76,11 +108,17 @@ void S4VRobot::wait_app_connection()
     Dabble.waitForAppConnection();
 }
 
+/**
+ * @brief Mô tả: Hàm kiểm tra tín hiệu điều khiển từ app Dabble trên điện thoại.
+ */
 void S4VRobot::processInput()
 {
     Dabble.processInput();
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển tay đòn của robot nâng lên phía trên.
+ */
 void S4VRobot::arm_upward()
 {
     if (armAngle < ARM_SERVO_MAX_ANGLE) {
@@ -89,6 +127,9 @@ void S4VRobot::arm_upward()
     }
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển tay đòn của robot hạ xuống phía dưới.
+ */
 void S4VRobot::arm_downward()
 {
     if (armAngle > ARM_SERVO_MIN_ANGLE) {
@@ -97,7 +138,9 @@ void S4VRobot::arm_downward()
     }
 }
 
-
+/**
+ * @brief Mô tả: Hàm điều khiển càng gắp của robot thu lại.
+ */
 void S4VRobot::grasp()
 {
     if (gripperAngle < GRIPPER_SERVO_MAX_ANGLE) {
@@ -106,6 +149,9 @@ void S4VRobot::grasp()
     }
 }
 
+/**
+ * @brief Mô tả: Hàm điều khiển càng gắp của robot mở ra.
+ */
 void S4VRobot::release()
 {
     if (gripperAngle > GRIPPER_SERVO_MIN_ANGLE) {
@@ -114,6 +160,12 @@ void S4VRobot::release()
     }
 }
 
+/**
+ * @brief Mô tả: Hàm nội bộ điều khiển riêng cho động cơ.
+ * @param motor: Motor bên trái (LEFT_MOTOR) hoặc motor bên phải (RIGHT_MOTOR).
+ * @param direction: Hướng xoay động cơ thuận chiều kim đồng hồ (CLOCKWISE) hay ngược chiều kim đồng hồ (COUNTER_CLOCKWISE).
+ * @param speed: Tốc độ của động cơ (0 - 100%).
+ */
 void S4VRobot::control_motor(Motor motor, Direction direction, uint8_t speed)
 {
     if (motor == LEFT_MOTOR) {
